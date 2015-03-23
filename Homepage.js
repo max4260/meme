117cb3bc1811365ece868db21e2a866c49446c2f
max4260 <hodpatf@gmail.com>
2015-03-23 10:33:17 -0500

    First Committed Update

---
+++ Homepage.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("button").click(function(){
+        $.POST("https://api.imgflip.com/caption_image",
+		{ 
+			template_id: "19723863",
+			username: "PHodges",
+			password: "Zo43671111Df",
+			text0: "BLAH",
+			text1: "BLAH"
+		},
+        function(data,status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
+</script>
+</head>
+<body>
+
+<button>Send</button>
+
+</body>
+</html>
